5adf617e10cc4e04308ed4696ed306a256f51f1a
mranjith323 <mranjith323@gmail.com>
2024-06-23 17:48:07 +0530

    remote-exec updates

--- terraform/deploy/ec2.tf
+++ terraform/deploy/ec2.tf
@@ -2,7 +2,7 @@ data "aws_ami" "ubuntu" {
      most_recent = true
      filter {
         name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server*"]
+        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
  }
 
      filter {
@@ -75,8 +75,8 @@ resource "aws_instance" "strapi-server" {
       "sudo apt update -y",
       "curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh",
       "sudo bash nodesource_setup.sh",
-      "sudo apt-get update -y",
-      "sudo apt-get install nodejs -y",
+      "sudo apt update -y",
+      "sudo apt install nodejs -y",
       "sudo apt install npm -y",
       "sudo npm install pm2@latest -g",
       "cd /srv/",
